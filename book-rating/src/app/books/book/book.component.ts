a86e992fb3146208dfa3ad6ea3698cd309d8e09d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-01 10:03:33 +0200

    performance profiling and Optimierung

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -9,10 +9,28 @@ import { Book } from '../shared/book';
 })
 export class BookComponent  {
 
+  // benötigt laut Profiler 1.4ms
+  /*
   @Input() book?: Book;
 
   get amountOfStars(): undefined[] {
     return new Array(this.book?.rating);
+  }
+  */
+
+  // benötigt laut Profiler 1ms (voll schneller 😎)
+  // ABER: geht nur wenn Book nicht mutiert wird!
+  _book?: Book;
+  amountOfStars: undefined[] = [];
+
+  @Input()
+  set book(book: Book | undefined) {
+    this._book = book;
+    this.amountOfStars = new Array(book?.rating)
+  }
+
+  get book(): Book | undefined {
+    return this._book;
   }
 
 }
