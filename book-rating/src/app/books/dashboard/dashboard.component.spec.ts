832a1d9a8d8db3078d63bdd45394c7d68676d328
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-04 12:10:35 +0200

    Integration-Test

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookComponent } from '../book/book.component';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -8,7 +9,10 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [
+        DashboardComponent,
+        BookComponent // Integration-Test
+      ]
     })
     .compileComponents();
 
