d3e94f0216b8173ac877d61380ee049907f2fcbb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-06 13:52:18 +0200

    rxjs: async!!

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,51 +1,14 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, take } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, take, tap } from 'rxjs';
 
 @Component({
   selector: 'rxw-unsubscribe',
   templateUrl: './unsubscribe.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnsubscribeComponent implements OnDestroy {
+export class UnsubscribeComponent  {
 
-  logStream$ = new ReplaySubject<string | number>();
-  destroy$ = new Subject();
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt ...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen ...
-   */
-  constructor() {
-    const interval$ = timer(0, 1000);
-
-    interval$.pipe(
-
-      /******************************/
-
-      // takeUntil(this.destroy$)
-      take(5)
-
-      /******************************/
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-  }
-
-  ngOnDestroy() {
-    this.logStream$.next('DESTROY');
-    this.destroy$.next(null);
-  }
-
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
+  interval$ = timer(0, 1000).pipe(
+    tap(console.log)
+  )
 }
