8eb764bf6d7d6b7af948ea9990eb3834442634b4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-06 14:41:36 +0200

    rxjs: final sample

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, mergeMap, concatMap, switchMap, catchError, of } from 'rxjs';
+import { map, mergeMap, concatMap, switchMap, catchError, of, shareReplay, retry } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -11,18 +11,21 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class BookDetailsComponent {
 
-
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
     switchMap(isbn => this.bs.getSingleBook(isbn!).pipe(
+      retry(3),
       catchError((err: HttpErrorResponse) => of({
         title: 'FEHLER',
         description: err.message
       }))
     )),
+    // shareReplay(1)
   );
 
-  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+
+  }
 
 
 }
