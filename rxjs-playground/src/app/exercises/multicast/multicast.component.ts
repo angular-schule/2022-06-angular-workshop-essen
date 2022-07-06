54d9c6cee382702306815efab29b5eb0351e5f18
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-06 13:30:09 +0200

    rxjs: hot vs cold

--- rxjs-playground/src/app/exercises/multicast/multicast.component.ts
+++ rxjs-playground/src/app/exercises/multicast/multicast.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil } from 'rxjs';
+import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil, shareReplay } from 'rxjs';
 
 import { MeasureValuesService } from './measure-values.service';
 import { ExerciseService } from '../exercise.service';
@@ -14,11 +14,30 @@ export class MulticastComponent implements OnDestroy {
   logStream$ = new ReplaySubject<string>();
   private destroy$ = new Subject<void>();
 
-  measureValues$: Observable<number>; // später: Subject<number>;
+  measureValues$: Subject<number>; // später: Subject<number>;
 
   constructor(private mvs: MeasureValuesService, private es: ExerciseService) {
+
     /**************!!**************/
-    this.measureValues$ = this.mvs.getValues();
+
+    // 1. unverändert (kalt)
+    // this.measureValues$ = this.mvs.getValues();
+
+
+    // 2. multicasts (shares) the original Observable, uses Subject internally
+    // this.measureValues$ = this.mvs.getValues().pipe(share())
+
+    // 3. multicasts (shares) the original Observable, uses ReplaySubject internally
+    // this.measureValues$ = this.mvs.getValues().pipe(shareReplay(1))
+
+    // 4. please try out:
+    // - Subject
+    // - ReplaySubject
+    // - BehaviorSubject
+    this.measureValues$ = new Subject<number>();
+    this.mvs.getValues().subscribe(this.measureValues$);
+
+
     /**************!!**************/
 
   }
